92b519045db27cdfe3b760d924182ce84feb197c
UsaaryanByte07 <usaaryan79@gmail.com>
2024-11-27 19:25:11 +0530

    Corrected a small logical error in Question No. 4

--- Practice Set/Chapter3/Question4.c
+++ Practice Set/Chapter3/Question4.c
@@ -6,7 +6,7 @@ int main(){
     int y;
     printf("Enter the Year : ");
     scanf("%d",&y);
-    if(y%4==0 && (y%100!=0 || y%400==0)){
+    if((y%4==0 && y%100!=0) || y%400==0){
         printf("The Year is a Leap year");
     } else{
         printf("The Year is not a Leap year");
